8327b88f07b0c09291e20c458c129a58de63b976
cran <name.is.carl@gmail.com>
2015-02-08 14:50:40 +0100

    Work

--- hen.sh
+++ hen.sh
@@ -2,14 +2,15 @@
 # 
 OPERATION="build"
 
-DEST_FOLDER=cmake-test
+DEST_FOLDER=./cmake-test
 TEMP_FOLDER=./hen-tmp
 SOURCE_URL=https://github.com/I-hate-zoos/hen/raw/master/dist/
 PACKAGE_NAME=hen-latest.zip
 PACKAGE_URL=${SOURCE_URL}/${PACKAGE_NAME}
-VERSION_URL=${SOURCE_URL}/Hen-VERSION
+VERSION_URL=${SOURCE_URL}/Hen-VERSION.txt
 
 PACKAGE_FILE=${TEMP_FOLDER}/${PACKAGE_NAME}
+VERSION_FILE=${TEMP_FOLDER}/Hen-VERSION.txt
 
 if [ -n "$1" ]; then
     OPERATION=$1
@@ -21,11 +22,25 @@ do_update () {
     	rm -rf ${TEMP_FOLDER}
     fi
     mkdir ${TEMP_FOLDER} 
-    wget -P ${TEMP_FOLDER} ${PACKAGE_URL}
+    echo "Checking for newer version..."
+    wget --quiet -P ${TEMP_FOLDER} ${VERSION_URL}
+    SERVER_VERSION=`cat ${VERSION_FILE}`
+    if [ -d ${DEST_FOLDER} ] ; then
+    	LOCAL_VERSION=`cat ${DEST_FOLDER}/Hen-VERSION.txt`
+    fi 
+    echo " Server version: ${SERVER_VERSION}"
+    echo " Local version : ${LOCAL_VERSION}"
+    echo "Getting the new files from server..."
+    wget --quiet -P ${TEMP_FOLDER} ${PACKAGE_URL}
     if [ -d ${DEST_FOLDER} ] ; then
     	rm -rf ${DEST_FOLDER}
     fi 
-    unzip ${PACKAGE_FILE} -d ${DEST_FOLDER}
+    unzip -q ${PACKAGE_FILE} -d ${DEST_FOLDER}
+
+    # Cleaning up
+    rm -rf ${TEMP_FOLDER}
+
+    echo "Hen updated to ${SERVER_VERSION}"
 }
 
 do_prepare () {
@@ -41,7 +56,7 @@ do_help () {
 }
 
 case "$OPERATION" in
-   "update") do_udpdate  
+   "update") do_update  
    ;;
    "prepare") do_prepare
    ;;
